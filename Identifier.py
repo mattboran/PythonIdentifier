af792ea80e5ad6c53c198350ef26dc25cb7f05be
Matt Boran <mattboran@gmail.com>
2016-08-10 15:05:52 -0400

    Started working.

--- Identifier.py
+++ Identifier.py
@@ -1,2 +1,54 @@
-import os
-filenames = next(os.walk(path))[2]
+import os, random
+
+def get_filenames(dir):
+	'''
+		Gets all filenames (no path) in a directory dir
+	'''
+	filenames = []
+	for root, directories, files in os.walk(dir):
+		for filename in files:
+			#filepath = os.path.join(root, filename)
+			filenames.append(filename)
+	return filenames
+
+def get_authornames(fnames):
+	'''
+		This function gets the names of the authors from the filename
+	'''
+	authnames = []
+	for f in fnames:
+		title = f.rsplit("_")
+		auth = title[len(title)-1]
+		authnames.append(auth[0:-4])
+	return authnames
+	
+def get_numwords(fname):
+	'''
+		This function gets the number of words in the book
+	'''
+	i = 0
+	f = open(fname, 'r')
+	for word in f:
+		i = i + 1
+	return i
+
+def count_instance(w, block):
+	'''
+		Gets the number of occurances of "w" in block
+	'''
+	i = 0
+	for word in block:
+		if w in word:
+			i = i + 1
+	return i
+	
+def count_instance_between(start, end, word, fname):
+	return 0
+	
+	
+filenames = get_filenames("Book txt")
+authnames = get_authornames(filenames)
+
+fullfilenames = []
+for f in filenames:
+	fullfilenames.append("Book txt\\" + f)
