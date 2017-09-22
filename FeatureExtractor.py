4f815b942b23dc3da144f1b24482dc91b7e5aac4
mattboran@gmail.com <mattboran@gmail.com>
2017-09-22 01:19:06 -0400

    Added time elapsed

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,4 +1,4 @@
-import sys, getopt, random
+import sys, getopt, random, time
 from Identifier import *
 
 '''
@@ -24,49 +24,50 @@ iterations = int(sys.argv[2])
 
 outputfilename = str(sys.argv[3])
 
-out_file = open(outputfilename, 'w')
-out_file.write(''.join(featnames))
 filenames = get_filenames(directory)
 authnames = get_authornames(filenames)
 
 fullfilenames = []
 for f in filenames:
 	fullfilenames.append(directory + f)
-	
-out_line = ''
 
+out_file = open(outputfilename, 'w')
+out_file.write(''.join(featnames))
+out_line = ''
+start_time = time.time()
 #now we've assembled our filenames, and know their true class. Let's iterate over each one N times then
 for f in fullfilenames:
-	
-	num_words = 0
-	file = open(f, 'r')
-	print "Working with " + f + " as our file. "
-	for line in file:
-		words = line.split()
-		num_words += len(words)
-	
-	for i in range(0, iterations):
-		charstream = get_block_by_char(f, random.randint(0, num_words-1500))
-		out_line += (str(get_num_paragraphs(charstream)) + ', ')
-		out_line += (str(get_char_per_line(charstream)) + ', ')
-		out_line += (str(count_pronouns(charstream)) + ', ')
-		out_line += (str(count_words_ending_in(charstream, "ing")) + ', ')
-		out_line += (str(count_word(charstream,"!")) + ', ')
-		out_line += (str(count_word(charstream,",")) + ', ')
-		out_line += (str(count_words_ending_in(charstream, "ly")) + ', ')
-		out_line += (str(count_word(charstream,"-")) + ', ')
-		out_line += (str(count_alliteration(charstream)) + ', ')
-		longWords = count_long_words(charstream)
-		shortWords = count_short_words(charstream)
-		longShortRatio = float(longWords)/float(shortWords)
-		out_line += (str(longShortRatio) + ', ')
-		out_line += (str(longWords) + ', ')
-		out_line += (str(count_word(charstream,":")) + ', ')
-		out_line += (str(count_word(charstream,";")) + ', ')
-		out_line += get_authorname(f)
-		out_line += '\n'
-		out_file.write(out_line)
-		out_line = ''
-			
+        
+        num_words = 0
+        file = open(f, 'r')
+        #print("Working with " + f + " as our file. ")
+        for line in file:
+                words = line.split()
+                num_words += len(words)
+        
+        for i in range(0, iters):
+                charstream = get_block_by_char(f, random.randint(0, num_words-1500))
+                out_line += (str(get_num_paragraphs(charstream)) + ', ')
+                out_line += (str(get_char_per_line(charstream)) + ', ')
+                out_line += (str(count_pronouns(charstream)) + ', ')
+                out_line += (str(count_words_ending_in(charstream, "ing")) + ', ')
+                out_line += (str(count_word(charstream,"!")) + ', ')
+                out_line += (str(count_word(charstream,",")) + ', ')
+                out_line += (str(count_words_ending_in(charstream, "ly")) + ', ')
+                out_line += (str(count_word(charstream,"-")) + ', ')
+                out_line += (str(count_alliteration(charstream)) + ', ')
+                longWords = count_long_words(charstream)
+                shortWords = count_short_words(charstream)
+                longShortRatio = float(longWords)/float(shortWords)
+                out_line += (str(longShortRatio) + ', ')
+                out_line += (str(longWords) + ', ')
+                out_line += (str(count_word(charstream,":")) + ', ')
+                out_line += (str(count_word(charstream,";")) + ', ')
+                out_line += get_authorname(f)
+                out_line += '\n'
+                out_file.write(out_line)
+                out_line = ''
+                        
+print("Time elapsed: %f" % (time.time() - start_time))
 out_file.close()
 	
